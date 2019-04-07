e0f7732d94df1b874570ab0351fa86e25b5e6f24
zhangwenfeng <zhangwenfeng@haizhi.com>
2019-04-07 12:32:29 +0800

    debug

--- src/pages/main/pay/PayCenter.vue
+++ src/pages/main/pay/PayCenter.vue
@@ -309,8 +309,7 @@ export default {
         'getBrandWCPayRequest',
         this.jsApiParameters,
         function(res){
-          alert(res.err_msg == 'get_brand_wcpay_request:ok');
-          if (res && res.err_msg && res.err_msg == "get_brand_wcpay_request:ok") {
+          if (res.err_msg == "get_brand_wcpay_request:ok") {
             alert('comein');
             // 支付成功 更改支付状态
             service_course.courseService
